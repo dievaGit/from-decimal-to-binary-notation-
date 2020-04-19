47734b62cbc8bfad9a521b4547b441351d70f841
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-19 15:08:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            uint n = 0;
+            string binary = "";
+           
+            //Requesting number
+            Console.Write(" Enter the positive number: ");
+            n = Convert.ToUInt32(Console.ReadLine());
+
+            binary = Convert.ToString(n, 2);
+
+            Console.Write(" Enter the number {0} as binary {1}", n, binary );
+
+            Console.ReadKey();
+        }
+    }
+}
